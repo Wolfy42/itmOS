2cb6fb96d9c1ba2c64f290fc476a9ccbfb7cca68
wolfi <halbeisen@hotmail.com>
2011-10-20 19:00:57 +0200

    removed files not belongig into revision history

--- BOSS/Debug/HAL/LED/LED.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-HAL/LED/LED.obj: ../HAL/LED/LED.cpp
-HAL/LED/LED.obj: ../HAL/LED/LED.h
-HAL/LED/LED.obj: ../HAL/LED/../gpio.h
-
-../HAL/LED/LED.cpp: 
-../HAL/LED/LED.h: 
-../HAL/LED/../gpio.h: 
--- BOSS/Debug/Kernel/TaskManagement/Task/TaskControlBlock.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-Kernel/TaskManagement/Task/TaskControlBlock.obj: ../Kernel/TaskManagement/Task/TaskControlBlock.cpp
-Kernel/TaskManagement/Task/TaskControlBlock.obj: ../Kernel/TaskManagement/Task/TaskControlBlock.h
-
-../Kernel/TaskManagement/Task/TaskControlBlock.cpp: 
-../Kernel/TaskManagement/Task/TaskControlBlock.h: 
