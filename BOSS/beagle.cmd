e28c508830dc35572db5c22b4a2c756fdcea5336
wolfi <halbeisen@hotmail.com>
2011-11-18 09:01:49 +0100

    fixed typing

--- BOSS/beagle.cmd
+++ BOSS/beagle.cmd
@@ -13,11 +13,12 @@ MEMORY
    int_ram:  ORIGIN = 0x40200000  LENGTH = 0x0000FFC4
    int_vecs: ORIGIN = 0x4020FFC4  LENGTH = 0x0000005B
    ext_ddr:  ORIGIN = 0x82000000  LENGTH = 0x00010000
+   
 }
 
 SECTIONS
 {
-   .intvecs   > int_vecs
+   .intvecs    > int_vecs
 		
    .const      > ext_ddr
    .bss        > ext_ddr
