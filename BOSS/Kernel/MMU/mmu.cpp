aa1189c1b6bd6d1689e98748dc37eca40afce00b
Tobias <tobis_mail@yahoo.de>
2011-11-19 08:14:15 +0100

    MMU is now using the kernelMastrerTable variable for placing the master table

--- BOSS/Kernel/MMU/mmu.cpp
+++ BOSS/Kernel/MMU/mmu.cpp
@@ -5,6 +5,7 @@ asm("\t .bss _taskMasterTableAddress, 4\n" \
     "\t .global _taskMasterTableAddress\n" \
     "taskMasterTableAddress .field _taskMasterTableAddress, 32");
 extern address taskMasterTableAddress;
+extern volatile unsigned int kernelMasterTable;
 address taskMasterTableAddresses[MAX_MMU_TABLES] = {0x0};
 
 bool occupiedAddresses[MAX_PAGES_IN_MEMORY] = {false};
@@ -14,18 +15,18 @@ void mmu_initMemoryForTask(int taskId) {
     if (taskMasterTableAddress == (address)0x0) {
         if (taskId == 0) {
             unsigned int i;
-            address tableAddress = (address)0x40200000;
-            taskMasterTableAddress = tableAddress;
+            taskMasterTableAddress = &kernelMasterTable;
             // Set Domain Access control register to 0101 0101 0101 0101 0101 0101 0101 0111 
             asm("\t MOV r1, #0x5557\n");
             asm("\t MOVT r1, #0x5555\n");
             asm("\t MCR p15, #0, r1, c3, c0, #0\n");
             
             // Set the Master Table Pointer to the internal ram
-            asm("\t MOV r1, #0x0000\n");
-            asm("\t MOVT r1, #0x4020\n");
+            asm("\t LDR r1, taskMasterTableAddress\n");
+            asm("\t LDR r1, [r1]\n");
             asm("\t MCR p15, #0, r1, c2, c0, #0\n");
             
+            address tableAddress = taskMasterTableAddress;
             // Initialize Master Table
             for (i = 0x00000000; i < 0xFFF00000; i += 0x00100000) {
                 *tableAddress = i | 0xC12;
--- BOSS/Kernel/MMU/mmu.h
+++ BOSS/Kernel/MMU/mmu.h
@@ -2,7 +2,8 @@
 #define MMU_H_
 
 #define MAX_MMU_TABLES 10
-#define MAX_PAGES_IN_MEMORY ((32 * 1024) / 4); // 32MB mapped to 4KB pages
+#define MAX_PAGES_IN_MEMORY 8192 // 32MB mapped to 4KB pages
+
 void mmu_initMemoryForTask(int taskId);
 void mmu_loadPage(int pageNumber);
 
