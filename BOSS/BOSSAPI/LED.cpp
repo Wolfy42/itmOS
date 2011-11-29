f1e0dae3267f78551f06e323ac332d93fd0c04ff
wolfi <halbeisen@hotmail.com>
2011-11-29 21:07:22 +0100

    send nr of parameters with the service-call

--- BOSS/BOSSAPI/LED.cpp
+++ BOSS/BOSSAPI/LED.cpp
@@ -3,20 +3,20 @@
 #include "serviceCalls.h"
 
 void toggleLED(LED led) {
-    int params[] = {LED_SERVICE_CALL, led, SERVICE_TOGGLE_LED};
+    int params[] = {LED_SERVICE_CALL, 2, led, SERVICE_TOGGLE_LED};
     performServiceCall(params);
 }
 void switchLEDOn(LED led) {
-    int params[] = {LED_SERVICE_CALL, led, SERVICE_SWITCH_LED_ON};
+    int params[] = {LED_SERVICE_CALL, 2, led, SERVICE_SWITCH_LED_ON};
     performServiceCall(params);
 }
 void switchLEDOff(LED led) {
-    int params[] = {LED_SERVICE_CALL, led, SERVICE_SWITCH_LED_OFF};
+    int params[] = {LED_SERVICE_CALL, 2, led, SERVICE_SWITCH_LED_OFF};
     performServiceCall(params);
 }
 
 bool getLEDState(LED led) {
-    int params[] = {LED_SERVICE_CALL, led, SERVICE_GET_LED_STATUS};
+    int params[] = {LED_SERVICE_CALL, 2, led, SERVICE_GET_LED_STATUS};
     performServiceCall(params);
     return false;
 }
