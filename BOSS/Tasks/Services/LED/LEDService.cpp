2ff4fcce7672b9e74fde19cef655d5cbbaacee8c
wolfi <halbeisen@hotmail.com>
2011-12-02 12:06:44 +0100

    refactored hal-led-driver

--- BOSS/Tasks/Services/LED/LEDService.cpp
+++ BOSS/Tasks/Services/LED/LEDService.cpp
@@ -15,20 +15,20 @@ void LEDService::executeMessage(Message* message)  {
 	LEDServiceCommand command = (LEDServiceCommand)params[1];
 
 	switch (command) {
-	        case SERVICE_SWITCH_LED_OFF:
-	        	_driver.ledOff(led);
-	            break;
-	        case SERVICE_SWITCH_LED_ON:
-	        	_driver.ledOn(led);
-	            break;
-	        case SERVICE_TOGGLE_LED:
-	        	_driver.toggle(led);
-	            break;
-	        case SERVICE_GET_LED_STATUS:
-	            int response[2];
-	            response[0] = params[2]; // write Back caller-Id
-	            response[1] = _driver.isOn(led);
-	            writeResponse(response);
-	            break;
-	    }
+		case SERVICE_SWITCH_LED_OFF:
+			_driver.ledOff(led);
+			break;
+		case SERVICE_SWITCH_LED_ON:
+			_driver.ledOn(led);
+			break;
+		case SERVICE_TOGGLE_LED:
+			_driver.toggle(led);
+			break;
+		case SERVICE_GET_LED_STATUS:
+			int response[2];
+			response[0] = params[2]; // write Back caller-Id
+			response[1] = _driver.isOn(led);
+			writeResponse(response);
+			break;
+	}
 }
