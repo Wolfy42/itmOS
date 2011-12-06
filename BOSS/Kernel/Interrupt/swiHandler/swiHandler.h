9b74c3bb8180d886b770f9bdeca4d147ea971f09
martin burtscher <martin.burtscher@students.fhv.at>
2011-12-06 13:38:11 +0100

    deleted unused folders

--- BOSS/Kernel/Interrupt/swiHandler/swiHandler.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SWIHANDLER_H_
-#define SWIHANDLER_H_
-
-#include "Kernel/SoftwareInterrupts/SWIExecutor.h"
-
-	void swi_setSWIExecutor(SWIExecutor* swiExecutor);
-
-#endif /* SWIHANDLER_H_ */
