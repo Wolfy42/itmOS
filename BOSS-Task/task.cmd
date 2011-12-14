cbb5921d9baf75b351e850fc9beb8c036b394304
wolfi <halbeisen@hotmail.com>
2011-12-14 09:57:37 +0100

    modified task-linker-file

--- BOSS-Task/task.cmd
+++ BOSS-Task/task.cmd
@@ -8,25 +8,30 @@
 
 MEMORY
 {
-   v_memory:     ORIGIN 0x00001000 LENGTH = 0x01000000
-   stack_memory: ORIGIN 0x10000000 LENGTH = 0x00002000
+   virtual_memory:     ORIGIN 0x00001000 LENGTH = 0x01000000
+   stack_memory:       ORIGIN 0x10000000 LENGTH = 0x00002000
+   messages_memory:    ORIGIN 0x20000000 LENGTH = 0x00001000
 }
 
 SECTIONS
 {
    ORDER
-   .text	   > v_memory
-   .bss        > v_memory
-   .const      > v_memory
-   .cinit      > v_memory
-   .pinit      > v_memory
-   .cio        > v_memory
-   .switch     > v_memory
-   .far        > v_memory
-   .data       > v_memory
-   .switch     > v_memory
-   .init_array > v_memory
-   .sysmem     > v_memory
+   .text	   > virtual_memory
+   .bss        > virtual_memory
+   .const      > virtual_memory
+   .cinit      > virtual_memory
+   .pinit      > virtual_memory
+   .cio        > virtual_memory
+   .switch     > virtual_memory
+   .far        > virtual_memory
+   .data       > virtual_memory
+   .switch     > virtual_memory
+   .init_array > virtual_memory
+   .sysmem     > virtual_memory
    
    .stack      > stack_memory
+   
+   .messages   > messages_memory  {
+   		_messagesStart = .;
+   }
 }
