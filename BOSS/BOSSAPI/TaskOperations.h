453621cf3614ef2718a2acab671af2e41a5bdd5d
martin burtscher <martin.burtscher@students.fhv.at>
2011-12-19 13:32:33 +0100

    metadata to gitignore && TaskOperations added comment

--- BOSS/BOSSAPI/TaskOperations.h
+++ BOSS/BOSSAPI/TaskOperations.h
@@ -2,6 +2,7 @@
 #define TASKOPERATIONS_H_
 
 //TODO: The User will not call yield. He will call newTask(...)
+//TODO: wenn newTask immer taskwechsel macht -> viel spass beim 100 tasks spawnen o_O
 void yield();
 void exit(int state);
 void kill(int pid);
