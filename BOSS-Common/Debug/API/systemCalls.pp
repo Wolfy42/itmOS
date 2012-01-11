8b98ea13be211b85a4f5bcfe94a89d3827e548b1
wolfi <halbeisen@hotmail.com>
2012-01-11 10:14:03 +0100

    deleted debug (again)

--- BOSS-Common/Debug/API/systemCalls.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-API/systemCalls.obj: ../API/systemCalls.cpp
-API/systemCalls.obj: C:/Users/root/Documents/itmOS/BOSS-Common/API/systemCalls.h
-API/systemCalls.obj: C:/Users/root/Documents/itmOS/BOSS-Common/API/dataTypes.h
-
-../API/systemCalls.cpp: 
-C:/Users/root/Documents/itmOS/BOSS-Common/API/systemCalls.h: 
-C:/Users/root/Documents/itmOS/BOSS-Common/API/dataTypes.h: 
--- BOSS-Common/Debug/Lib/bitOperations.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-Lib/bitOperations.obj: ../Lib/bitOperations.cpp
-Lib/bitOperations.obj: ../Lib/bitOperations.h
-Lib/bitOperations.obj: C:/Users/root/Documents/itmOS/BOSS-Common/API/dataTypes.h
-
-../Lib/bitOperations.cpp: 
-../Lib/bitOperations.h: 
-C:/Users/root/Documents/itmOS/BOSS-Common/API/dataTypes.h: 
