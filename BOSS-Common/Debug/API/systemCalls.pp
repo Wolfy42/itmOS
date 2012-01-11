b350c0655f392ce18dbfb438d36b41747a0722c8
Stefan Gassner <stefan.gassner@gmail.com>
2012-01-11 17:27:45 +0100

    added smoke on the water intro on startup - 3 times loop

---
+++ BOSS-Common/Debug/API/systemCalls.pp
@@ -0,0 +1,7 @@
+# FIXED
+
+API/systemCalls.obj: ../API/systemCalls.cpp
+API/systemCalls.obj: /home/stefan/BeagleBoard/itmOS/BOSS-Common/API/systemCalls.h
+
+../API/systemCalls.cpp: 
+/home/stefan/BeagleBoard/itmOS/BOSS-Common/API/systemCalls.h: 
