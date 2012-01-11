da3c173a6a13075e72159d0c3141a2052df973d2
Stefan Gassner <stefan.gassner@gmail.com>
2012-01-11 13:33:01 +0100

    Audio done

--- BOSS/Kernel/main.cpp
+++ BOSS/Kernel/main.cpp
@@ -15,7 +15,7 @@ int printBOSS();
 int main() {
 	_disable_interrupts();
 
-	// Audio Test -> "berühr dass, und eine Zahnbürste fährt morgens ins Leere!"
+	// Audio Test -> "berühr dass, und deine Zahnbürste fährt morgens ins Leere!"
 	Audio* audio = new Audio();
 	audio->playSample();
 	delete audio;
