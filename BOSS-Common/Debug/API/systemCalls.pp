43337386d16ed5a21af238cf5ced562a39e48fea
wolfi <halbeisen@hotmail.com>
2012-01-12 08:07:02 +0100

    .gitignore is now working

--- BOSS-Common/Debug/API/systemCalls.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-API/systemCalls.obj: ../API/systemCalls.cpp
-API/systemCalls.obj: /home/stefan/BeagleBoard/itmOS/BOSS-Common/API/systemCalls.h
-
-../API/systemCalls.cpp: 
-/home/stefan/BeagleBoard/itmOS/BOSS-Common/API/systemCalls.h: 
