7b9857a8fd7d8c92f53ca96be4c2496776e1fc3d
martin burtscher <martin.burtscher@students.fhv.at>
2012-01-12 10:35:28 +0100

    added Shell Service v1 - still missing HDMI Service

---
+++ BOSS-Task/Tasks/Services/Shell/ShellService.cpp
@@ -0,0 +1,61 @@
+#include "ShellService.h"
+
+
+
+void ShellService::executeMessage(Message* message)  {
+
+	int* params = message->getParams();
+	char* char_params;
+	
+	for (int i = 1; i < message->getParamsLength(); i++) {
+	
+		char_params[i - 1] = params[i];	
+	}
+
+	// read or write?
+	ShellCommand command = (ShellCommand)params[0];
+	
+
+
+	switch (command) {
+		case NEXT_LETTER:
+		
+			if (char_params[0] == '\n' || char_params[0] == '\r') {
+			
+				executeCommand(input);
+				input = "";
+			} else {
+			
+				input.append(&char_params[0]);	
+			}
+			break;
+	}
+}
+
+
+void ShellService::executeCommand(std::string command) {
+
+	if (std::strcmp("exit", command.c_str()) == 0) {
+		
+		echo("can't shut down the one and only shell.");
+	} else if (std::strcmp("help", command.c_str()) == 0) {
+	
+		echo("if you're using BOSS you should be a fkn geek and wouldn't need any help.");
+	}
+}
+
+void ShellService::echo(std::string echo) {
+	
+	int params[] = { (int)echo.c_str() };
+	performServiceCall(HDMI_SERVICE_ID, 2, params);
+}
+
+ShellService::ShellService()
+{
+
+}
+
+ShellService::~ShellService()
+{
+}
+
---
+++ BOSS-Task/Tasks/Services/Shell/main.cpp
@@ -0,0 +1,6 @@
+#include "Tasks/Services/Shell/ShellService.h"
+
+int ______main(void) {
+	ShellService shellService;
+	shellService.run();
+}
